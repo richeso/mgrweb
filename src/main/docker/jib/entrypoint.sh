a3ff0b356f1badf0d65fbf514db2cedeb8ce08b6
Richard So <richard.cheso@gmail.com>
2021-10-25 11:22:59 -0400

    change menu text colors

--- src/main/docker/jib/entrypoint.sh
+++ src/main/docker/jib/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-echo "192.168.8.54    demo.mapr.com demo" >> /etc/hosts
-/opt/mapr/server/configure.sh -N demo.mapr.com -c -C 192.168.8.54:7222
+echo "10.163.169.55    rcstest55" >> /etc/hosts
+/opt/mapr/server/configure.sh -N rcs.cluster.com -c -C rcstest55:7222
 ###cp /app/resources/config/mapr-clusters.conf /opt/mapr/conf/mapr-clusters.conf
 ###sleep 1d
 ##exec java ${JAVA_OPTS} -noverify -XX:+AlwaysPreTouch -Djava.security.egd=file:/dev/./urandom -cp /app/resources/:/app/classes/:/app/libs/* "com.mapr.mgrweb.MswebApp"  "$@"
